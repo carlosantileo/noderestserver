4b622d29bc399ad8441c356a30f867547b3ffab2
Carlos Antileo <carlos.antileo.catelican@everis.com>
2020-05-11 18:40:42 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cantileo:9eWVbFJ2jziyLRvM@cluster0-wum3s.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.urlDB = urlDB;
